a166309bb3a047ff5a05b036649f53ef810a2d09
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-04-17 19:32:02 -0300

    Creating a examples of request and add cors on backend

--- API/src/server.ts
+++ API/src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -9,6 +10,7 @@ import swaggerDocs from "./swagger.json";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/users", userRoutes);
