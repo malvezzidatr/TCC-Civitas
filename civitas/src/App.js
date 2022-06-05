04539112505f71490976de4aba380c53d10a06a6
caio vinicius malvezzi <caiomalvezzi101@gmail.com>
2022-06-05 20:03:21 -0300

    Alterando o favicon

--- civitas/src/App.js
+++ civitas/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import React from "react";
+import { Helmet } from 'react-helmet';
 import './App.css';
 
 import Router from "./routes.js";
@@ -7,8 +8,13 @@ import Router from "./routes.js";
 
 function App() {
     // eslint-disable-next-line no-undef
+    const TITLE = 'Civitas'
     return (
         <div className="App">
+            <Helmet>
+                <title>{ TITLE }</title>
+                <link rel="shortcut icon" href="logo-laranja.ico" type="image/x-icon" />
+            </Helmet>
             <Router />
         </div>
     );
